12c270f4d2877f77a0db3e66f8d1456f9e2d2d55
Nahuel Agustin Dumo <nahueldumo2014@gmail.com>
2025-09-02 19:49:32 -0300

    chequeamos solucion

--- models/res_partner.py
+++ models/res_partner.py
@@ -5,14 +5,14 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    # is_design_user = fields.Boolean(
-    #     string='Usuario Diseño',
-    #     default=False,
-    #     help='Si está marcado, el usuario será redirigido automáticamente a /my/designs en el portal'
-    # )
+    is_design_user = fields.Boolean(
+        string='Usuario Diseño',
+        default=False,
+        help='Si está marcado, el usuario será redirigido automáticamente a /my/designs en el portal'
+    )
 
-    # def toggle_design_user(self):
-    #     """Alterna el estado de Usuario Diseño"""
-    #     for record in self:
-    #         record.is_design_user = not record.is_design_user
-    #     return True
+    def toggle_design_user(self):
+        """Alterna el estado de Usuario Diseño"""
+        for record in self:
+            record.is_design_user = not record.is_design_user
+        return True
